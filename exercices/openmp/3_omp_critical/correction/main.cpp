ecd6a2a215d56f6a737c89cd3715c18e580a6f7e
Mathieu Lobet <mathieu.lobet@cea.fr>
2020-12-07 16:14:41 +0100

    correction for the 3rd OpenMP exercise

--- exercices/openmp/3_omp_critical/correction/main.cpp
+++ exercices/openmp/3_omp_critical/correction/main.cpp
@@ -14,8 +14,8 @@
 int main( int argc, char *argv[] )
 {
 
-    int N = 100000000;
-    double sum = 0;
+    int N = 100;
+    double sum;
     
     #pragma omp parallel default(shared)
     {
@@ -24,18 +24,22 @@ int main( int argc, char *argv[] )
         
         double time_0 = omp_get_wtime();
     
-        // Chaque thread ajoute à sum sa contribution N fois
+        // Chaque thread traite son tableau
+
         for (int i = 0 ; i < N ; i++) {
+            #pragma omp atomic
             sum += cos(2*id*i);
         }
 
         double time_1 = omp_get_wtime();
         
+        // #pragma omp barrier
+        #pragma omp critical
         std::cerr << " Thread: " << id
                   << " - temps : " << time_1 - time_0 << " s" << std::endl;
         
     }
     
-    std::cerr << " Sum: " << sum << std::endl;
+    std::cout << " Sum: " << sum << std::endl;
     
 }
