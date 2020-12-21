796eec61a5254a467303e701c6a2facf71ac8a72
Mathieu Lobet <mathieu.lobet@cea.fr>
2020-12-21 11:37:22 +0100

    Marble: rename python folder visualization, mmove scaling scripts in folder scalings

--- tp/marble/scalings/strong_scaling_mpi_ppp2000.py
+++ tp/marble/scalings/strong_scaling_mpi_ppp2000.py
@@ -15,23 +15,23 @@ from matplotlib.pyplot import *
 times = {}
 
 # Nombre de processus
-times["cores"]       = np.array([])
+times["cores"]       = np.array([4,8,16,32,64])
 # Temps dans la boucle en temps
-times["loop"] = np.array([])
+times["loop"] = np.array([148.410497,37.865224,10.297101,3.131366,1.035853])
 # Temps pour les collisions
-times["collision"] = np.array([])
+times["collision"] = np.array([144.869179,36.129406,9.030512,2.272468,0.567437])
 # Temps dans le pousseur
-times["pusher"] = np.array([])
+times["pusher"] = np.array([0.067916,0.034055,0.017134,0.008759,0.004411])
 # Temps dans les echanges de particules
-times["exchange"] = np.array([])
+times["exchange"] = np.array([3.215714,1.562653,1.049943,0.590709,0.278090])
 # Temps dans les communications globales
-times["global"] = np.array([])
-
+times["global"] = np.array([0.013876,0.016409,0.137428,0.227660, 0.169374])
 
 
 # Calcul de l'efficacite pour une scalabilite faible
-times["efficiency"] = times["loop"][0] / times["loop"][:]
+times["efficiency"] = times["loop"][0] * times["cores"][0] / (times["loop"][:] * times["cores"][:])
 
+print(times["loop"][:] * times["cores"][:])
 
 # ______________________________________________________________________________
 # RCParams - pour ameliorer le rendu de la figure
@@ -90,7 +90,7 @@ ax.plot(times["cores"],times["efficiency"],lw=2,label="Simulation")
 
 ax.plot([times["cores"].min(), times["cores"].max()],[1,1],lw=2,label="Scalabilite parfaite")
 
-ax.set_ylim([0.,1.1])
+# ax.set_ylim([0.,1.1])
 
 ax.set_xlabel("Nombre de processus")
 ax.set_ylabel("Efficacite")
@@ -106,9 +106,9 @@ fig = figure(figsize=(12, 6))
 gs = GridSpec(2, 2)
 ax = subplot(gs[:,:])
 
-ax.plot(times["cores"],times["collision"]/times["loop"]*100.,lw=2,color='C1',label='part collision')
-ax.plot(times["cores"],times["exchange"]/times["loop"]*100.,lw=2,color='C2',label='part echange particules')
-ax.plot(times["cores"],times["global"]/times["loop"]*100.,lw=2,color='C3',label='part communications globales')
+ax.plot(times["cores"],times["collision"]/times["loop"]*100.,lw=2,color='C1',label='Collision')
+ax.plot(times["cores"],times["exchange"]/times["loop"]*100.,lw=2,color='C2',label='Echange particules')
+ax.plot(times["cores"],times["global"]/times["loop"]*100.,lw=2,color='C3',label='Communications globales')
 
 ax.set_xlabel("Nombre de processus")
 ax.set_ylabel("Part (%)")
