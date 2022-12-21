619c239e261886ba74f3835b560ab8393da81f54
xxirii <>
2022-12-22 00:15:50 +0100

    update maxwell tp

--- tp/maxwell/visualization/plot_binary.py
+++ tp/maxwell/visualization/plot_binary.py
@@ -79,18 +79,26 @@ By = np.array(struct.unpack('{}d'.format(size),content[k:k + 8*size])) ; k+= 8*s
 size = nxd*nyd
 Bz = np.array(struct.unpack('{}d'.format(size),content[k:k + 8*size])) ; k+= 8*size
 
-Ex_map = np.reshape(Ex, (nyp,nxd))
-Ey_map = np.reshape(Ey, (nyd,nxp))
-
 # Ez_map = np.zeros([nyp, nxp])
 # for iy in range(nyp):
 #     for ix in range(nxp):
 #         Ez_map[iy,ix] = Ez[iy*nxp+ix]
-Ez_map = np.reshape(Ez, (nyp,nxp))
 
-Bx_map = np.reshape(Bx, (nyd,nxp))
-By_map = np.reshape(By, (nyp,nxd))
-Bz_map = np.reshape(Bz, (nyd,nxd))
+# Ex_map = np.reshape(Ex, (nyp,nxd))
+# Ey_map = np.reshape(Ey, (nyd,nxp))
+# Ez_map = np.reshape(Ez, (nyp,nxp))
+
+# Bx_map = np.reshape(Bx, (nyd,nxp))
+# By_map = np.reshape(By, (nyp,nxd))
+# Bz_map = np.reshape(Bz, (nyd,nxd))
+
+Ex_map = np.reshape(Ex, (nxd,nyp))
+Ey_map = np.reshape(Ey, (nxp,nyd))
+Ez_map = np.reshape(Ez, (nxp,nyp))
+
+Bx_map = np.reshape(Bx, (nxp,nyd))
+By_map = np.reshape(By, (nxd,nyp))
+Bz_map = np.reshape(Bz, (nxd,nyd))
 
 xp = np.arange(nxp)
 yp = np.arange(nyp)
@@ -118,33 +126,33 @@ def symetric(im):
     max = 0.8*np.max(np.absolute(clim))
     im.set_clim([-max,max])
 
-im00 = ax00.pcolormesh(xd,yp,Ex_map,shading='auto',cmap=colormaps[colormap_name])
+im00 = ax00.pcolormesh(xd,yp,Ex_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im00)
 cb00 = colorbar(im00, ax=ax00)
 ax00.set_title("Ex (it: {})".format(iteration))
 #im00.set_clim([-1e-6,1e-6])
 
-im01 = ax01.pcolormesh(xp,yd,Ey_map,shading='auto',cmap=colormaps[colormap_name])
+im01 = ax01.pcolormesh(xp,yd,Ey_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im01)
 cb01 = colorbar(im01, ax=ax01)
 ax01.set_title("Ey")
 
-im02 = ax02.pcolormesh(xp,yp,Ez_map,shading='auto',cmap=colormaps[colormap_name])
+im02 = ax02.pcolormesh(xp,yp,Ez_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im02)
 cb02 = colorbar(im02, ax=ax02)
 ax02.set_title("Ez")
 
-im10 = ax10.pcolormesh(xp,yd,Bx_map,shading='auto',cmap=colormaps[colormap_name])
+im10 = ax10.pcolormesh(xp,yd,Bx_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im10)
 cb10 = colorbar(im10, ax=ax10)
 ax10.set_title("Bx")
 
-im11 = ax11.pcolormesh(xd,yp,By_map,shading='auto',cmap=colormaps[colormap_name])
+im11 = ax11.pcolormesh(xd,yp,By_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im11)
 cb11 = colorbar(im11, ax=ax11)
 ax11.set_title("By")
 
-im12 = ax12.pcolormesh(xd,yd,Bz_map,shading='auto',cmap=colormaps[colormap_name])
+im12 = ax12.pcolormesh(xd,yd,Bz_map.T,shading='auto',cmap=colormaps[colormap_name])
 symetric(im12)
 cb12 = colorbar(im12, ax=ax12)
 ax12.set_title("Bz")
