fba5411006dedd19f654d47bf757ff6485559213
xxirii <>
2023-01-03 16:59:49 +0100

    add run generator for maxwell

---
+++ tp/maxwell/scalings/launch_runs.py
@@ -0,0 +1,28 @@
+import os
+
+number_of_cores = [1,2,4,8,16,32,40,64,80]
+
+for cores in number_of_cores:
+
+    folder = "cores_{:02d}".format(cores)
+
+    os.makedirs(folder)
+
+    script = "#!/bin/bash \n"
+    script += "#SBATCH --job-name=master_dfe \n"
+    script += "#SBATCH --output=output \n"
+    script += "#SBATCH --error=error \n"
+    script += "#SBATCH --ntasks=80                # Nombre d'unité de calcul ou de processus MPI \n"
+    script += "#SBATCH --nodes=2                # Nombre d'unité de calcul ou de processus MPI \n"
+    script += "#SBATCH --time=00:10:00           # Temps souhaité pour la réservation \n"
+    script += "#SBATCH --partition=cpu_short     # Partition des jobs rapides \n"
+    script += "source ~/env_dfe_hpc.sh \n"
+    script += "et -x \n"
+    script += "cd ${SLURM_SUBMIT_DIR} \n"
+    script += "srun ../exe -it 2000 -nx 5120 -ny 128 -p 100 -d 100 \n"
+
+    fp = open("{}/launch.sh".format(folder), 'w')
+    fp.write(script)
+    fp.close()
+
+    os.system("sbatch {}/launch.sh".format(folder))
